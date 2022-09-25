7835d62805b16dabc152ac5184ffde9f8b5b409f
4B92B <113971701+4B92B@users.noreply.github.com>
2022-09-25 11:34:53 +0500

    Update Fuck.py

--- Fuck.py
+++ Fuck.py
@@ -69,7 +69,7 @@ class Main():
                 FG = '\x1b[0;92m'
                 GET = '\r'
             else:
-                key = '\x1b[0;91m -'3H90S2HOUPAA
+                key = '\x1b[0;91m 22
                 stat = '\x1b[0;91m Paid USER '
                 FY = '\x1b[0;90m'
                 FG = '\x1b[0;90m'
